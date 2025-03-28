4dcf1e98a16cd60e8d548c7c52b97d88bcad2416
RobiBlocks <142887221+RobiBlocks@users.noreply.github.com>
2025-03-28 15:53:21 +0100

    Add files via upload

--- tamagotchi.py
+++ tamagotchi.py
@@ -2,61 +2,32 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
 from tkinter import ttk
+from tkinter import Label
+from PIL import Image
+from PIL import ImageTk
 import random
 import sys
 
 window = tk.Tk()
+buttonFrame = tk.Frame(window)
+bg_color = "#87CEEB"
+
+fishStorage = 50
 
 def startGame():
-    # Fenstergröße
-    width = 1000
-    height = 500
+    createWindow()
+    createFrame()
+    createButtons()
+    createBars()
 
-    # Berechne die Position, um das Fenster zu zentrieren
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-
-    x_pos = (screen_width // 2) - (width // 2)
-    y_pos = ((screen_height - 100) // 2) - (height // 2)
-    
-    window.geometry(f"{width}x{height}+{x_pos}+{y_pos}")
-    window.title("Pinguin Tamagotchi")
-    window.bind("<KeyPress>", shortcut)
-
-    buttonFrame = tk.Frame(window)
-    buttonFrame.columnconfigure(0, weight=1)
-    buttonFrame.columnconfigure(1, weight=2)
-    buttonFrame.columnconfigure(2, weight=1)
-    buttonFrame.rowconfigure(0, weight=1)
-    buttonFrame.rowconfigure(1, weight=1)
-    buttonFrame.rowconfigure(2, weight=1)
-    buttonFrame.rowconfigure(3, weight=1)
+    window.configure(background=bg_color)
 
     image = tk.PhotoImage(file="pinguin.png")
     image = image.subsample(2, 2)
-    imageTamagotchi = tk.Label(buttonFrame, image=image)
-    imageTamagotchi.grid(row=1, rowspan=3, column=1, sticky="nswe")
+    imageTamagotchi = tk.Label(buttonFrame, image=image, bg=bg_color)
+    imageTamagotchi.grid(row=3, rowspan=3, column=1, columnspan=3, sticky="nswe")
 
-    buttonStreicheln = tk.Button(buttonFrame, text="Streicheln", font=('Arial', 12), bg="#ffff00", height=8, command=pet)
-    buttonStreicheln.grid(row=1, column=0, sticky="we")
-
-    buttonFeed = tk.Button(buttonFrame, text="Füttern", font=('Arial', 12), bg="#ff3c00", height=8, command=feed)
-    buttonFeed.grid(row=2, column=0, sticky="we")
-
-    buttonSleep = tk.Button(buttonFrame, text="Schlafen", font=('Arial', 12), bg="#008cff", height=8, command=sleep)
-    buttonSleep.grid(row=3, column=0, sticky="we")
-
-    global LoveProgressbar
-    LoveProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
-    LoveProgressbar.grid(row=1, column=2, sticky="we")
-
-    global HungerProgressbar
-    HungerProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
-    HungerProgressbar.grid(row=2, column=2, sticky="we")
-
-    global TirednessProgressbar
-    TirednessProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
-    TirednessProgressbar.grid(row=3, column=2, sticky="we")
+    load()
 
     buttonFrame.pack(fill="x", padx=20, pady=20)
 
@@ -68,11 +39,90 @@ def gameCycle():
     lowerStats()
     window.after(1000, gameCycle)
 
+def createWindow():
+    width = 800
+    height = 400
+
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_pos = (screen_width // 2) - (width // 2)
+    y_pos = ((screen_height - 100) // 2) - (height // 2)
+    
+    window.geometry(f"{width}x{height}+{x_pos}+{y_pos}")
+    window.title("Pinguin Tamagotchi")
+    window.bind("<KeyPress>", shortcut)
+
+def createFrame():
+    buttonFrame.columnconfigure(0, weight=1)
+    buttonFrame.columnconfigure(1, weight=1)
+    buttonFrame.columnconfigure(2, weight=1)
+    buttonFrame.columnconfigure(3, weight=1)
+    buttonFrame.columnconfigure(4, weight=1)
+
+    buttonFrame.rowconfigure(0, weight=1)
+    buttonFrame.rowconfigure(1, minsize=30)
+    buttonFrame.rowconfigure(2, weight=1)
+    buttonFrame.rowconfigure(3, weight=1)
+    buttonFrame.rowconfigure(4, weight=1)
+    buttonFrame.rowconfigure(5, weight=1)
+    
+    buttonFrame.configure(bg=bg_color)
+
+def createButtons():
+    buttonSave = tk.Button(buttonFrame, text="Speichern", font=('Arial', 12), height=6, command=save)
+    buttonSave.grid(row=0, column=0, sticky="we")
+
+    buttonLoad = tk.Button(buttonFrame, text="Laden", font=('Arial', 12), height=6, command=load)
+    buttonLoad.grid(row=0, column=1, sticky="we")
+
+    buttonFish = tk.Button(buttonFrame, text="Angeln", font=('Arial', 12), height=6, command=earnFish)
+    buttonFish.grid(row=0, column=4, sticky="we")
+
+    buttonStreicheln = tk.Button(buttonFrame, text="Streicheln", font=('Arial', 12), bg="#ffff00", height=8, command=pet)
+    buttonStreicheln.grid(row=3, column=0, sticky="we")
+
+    buttonFeed = tk.Button(buttonFrame, text="Füttern", font=('Arial', 12), bg="#ff3c00", height=8, command=feed)
+    buttonFeed.grid(row=4, column=0, sticky="we")
+
+    buttonSleep = tk.Button(buttonFrame, text="Schlafen", font=('Arial', 12), bg="#008cff", height=8, command=sleep)
+    buttonSleep.grid(row=5, column=0, sticky="we")
+
+def createBars():
+    global LoveProgressbar
+    LoveProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
+    LoveProgressbar.grid(row=3, column=4, sticky="we")
+
+    global HungerProgressbar
+    HungerProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
+    HungerProgressbar.grid(row=4, column=4, sticky="we")
+
+    global TirednessProgressbar
+    TirednessProgressbar = ttk.Progressbar(buttonFrame, mode="determinate", value=100)
+    TirednessProgressbar.grid(row=5, column=4, sticky="we")
+
+def load():
+    savefile = open('savefile.txt','r')
+    savedata = savefile.readlines()
+    LoveProgressbar['value'] = savedata[0].strip()
+    HungerProgressbar['value'] = savedata[1].strip()
+    TirednessProgressbar['value'] = savedata[2].strip()
+    savefile.close()
+
+def save():
+    savefile = open('savefile.txt','w')
+    savedata = f"{LoveProgressbar['value']}\n{HungerProgressbar['value']}\n{TirednessProgressbar['value']}"
+    savefile.write(savedata)
+    savefile.close()
+
 def pet():
     addValueToProgressbar(LoveProgressbar, 15)
 
 def feed():
+    global fishStorage
     addValueToProgressbar(HungerProgressbar, 20)
+    fishStorage -= 20
+    print(fishStorage)
 
 def sleep():
     messagebox.showinfo(title="Nachricht", message="Dein Pinguin ist eingeschlafen")
@@ -90,7 +140,6 @@ def removeValueFromProgressbar(progressbar, removeValue):
     progressbar["value"] = value
 
 def lowerStats():
-    
     if LoveProgressbar["value"] == 0:
         messagebox.showerror(title="Tod", message="Dein Pinguin ist wegen Liebesentzug gestroben!")
         sys.exit()  # Beendet das Skript
@@ -121,6 +170,14 @@ def generateIllness():
         removeValueFromProgressbar(LoveProgressbar, 3)
         removeValueFromProgressbar(HungerProgressbar, 2)
         removeValueFromProgressbar(TirednessProgressbar, 1)
+
+def earnFish():
+    global fishStorage
+    addFish = random.randint(1, 10)
+    while addFish == 1 or addFish == 2 or addFish == 4 or addFish == 6 or addFish == 8 or addFish == 9:
+        addFish = random.randint(1, 10)
+    fishStorage += addFish
+    print(fishStorage)
 
 def shortcut(event):
     if event.state == 12 and event.keysym == "p":
